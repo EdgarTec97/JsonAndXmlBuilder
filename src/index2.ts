df0a0a4dcb39c0b92b708efdd13f103ac399a6a1
Edgar Vega <vayne.edgar271@gmail.com>
2022-01-17 21:03:08 -0600

    fix: the combined object

--- src/index2.ts
+++ src/index2.ts
@@ -3,9 +3,9 @@ import object from "./object2";
 const objectC: any = object;
 const keys = Object.keys(object);
 
-let combined = {};
+let template = "[";
 
-keys.forEach((key) => {
+keys.forEach((key, index) => {
   const fixedKey = key.split(".");
   let templateString = "{";
 
@@ -22,18 +22,29 @@ keys.forEach((key) => {
     }
   });
 
-  templateString += `${objectStructred}}`;
-
-  objCombine(JSON.parse(templateString), combined);
+  templateString +=
+    keys.length == index + 1 ? `${objectStructred}}` : `${objectStructred}},`;
+  template += templateString;
 });
-
-console.log(JSON.stringify(combined));
+template += "]";
+const array: any[] = JSON.parse(template);
+let combined = {};
+// array.forEach((element) => {
+//   objCombine(element, combined);
+// });
+objCombine(array[0], combined);
+objCombine(array[1], combined);
+objCombine(array[2], combined);
+objCombine(array[3], combined);
+objCombine(array[4], combined);
+console.log(JSON.stringify(combined, null, 1));
 
 function objCombine(obj: any, variable: any) {
   for (let key of Object.keys(obj)) {
     if (!variable[key]) variable[key] = {};
 
-    for (let innerKey of Object.keys(obj[key]))
+    for (let innerKey of Object.keys(obj[key])) {
       variable[key][innerKey] = obj[key][innerKey];
+    }
   }
 }
